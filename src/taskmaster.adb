902a5a0931beee688da3e5c2b9b51a440f3453ff
Peter Hutkins <peter.hutkins@glydways.com>
2024-04-24 12:47:44 -0700

    Apply Ravenscar profile to all files within project through build-time pragrams

--- src/taskmaster.adb
+++ src/taskmaster.adb
@@ -1,4 +1,3 @@
-pragma Profile (Ravenscar);
 
 with Ada.Text_IO; use Ada.Text_IO;
 with Task_Manager; use Task_Manager;
@@ -7,7 +6,7 @@ procedure Taskmaster is
 begin
    Put_Line ("This is the first line from Ada.");
 
-   Task_Manager.Task1_Trigger.Activate;
+      Task_Manager.Task1_Trigger.Activate;
 
    Put_Line ("This message is from Ada.");
 end Taskmaster;
