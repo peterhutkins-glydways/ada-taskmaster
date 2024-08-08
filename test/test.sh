aa9d4d93e721da7e2501e2db9f6413c51171e515
Peter Hutkins <peter.hutkins@glydways.com>
2024-08-08 10:53:08 -0700

    test.sh -- updated to find the 'gnatmake' within alr's installed toolset

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This 'eval' exports alr's environment variables into the environment of the script.
+# Specifically, it sets the PATH to find the binaries like "gnatmake"
+eval $(alr -n -q printenv --unix)
+
 gnatmake -D obj -aI../src test_callables_args.adb -vh
 if [ $? == 0 ]; then
   ./test_callables_args
