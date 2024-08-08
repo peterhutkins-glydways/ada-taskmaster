9e48da2aca6a8e580df7ff50c7c716365d6344c5
Peter Hutkins <peter.hutkins@glydways.com>
2024-08-08 11:53:36 -0700

    improves test.sh. Ensures there is an obj/ directory. I think this is a little better than having an obj/.gitkeep file, as I can now  'rm -rf obj' to do a clean build.

--- test/test.sh
+++ test/test.sh
@@ -4,6 +4,11 @@
 # Specifically, it sets the PATH to find the binaries like "gnatmake"
 eval $(alr -n -q printenv --unix)
 
+# Make obj directory if it doesn't exist
+mkdir -p obj
+
+# This compiles only the one source file and its dependencies.
+# Note that it does NOT apply the usual compile switches from the project file.
 gnatmake -D obj -aI../src test_callables_args.adb -vh
 if [ $? == 0 ]; then
   ./test_callables_args
